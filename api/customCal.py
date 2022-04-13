86e9d0a458f00b4d2c63adb917de56877f757916
Mustfa Ayyed Alhasanat <mustfaaayyed@gmail.com>
2022-04-13 12:26:23 +0300

    resub 4

--- api/customCal.py
+++ api/customCal.py
@@ -10,9 +10,8 @@ class handler(BaseHTTPRequestHandler):
         query_string_list = parse.parse_qsl(url_components.query)
         dic = dict(query_string_list)
         
-        print(dic)
-        
         month, year = dic.get("month"), dic("year")
+        month, year = int(month), int(year)
         
         if (type(month) == int) and (type(year) == int):
             message = calendar.month(year, month)
