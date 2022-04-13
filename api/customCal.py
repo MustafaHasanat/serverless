f1549556c3bd2d744d3f6d6cfd54175d393ae0b8
Mustfa Ayyed Alhasanat <mustfaaayyed@gmail.com>
2022-04-13 11:40:05 +0300

    resub

---
+++ api/customCal.py
@@ -0,0 +1,29 @@
+import calendar
+from http.server import BaseHTTPRequestHandler
+from urllib import parse
+
+
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        s = self.path
+        url_components = parse.urlparse(s)
+        query_string_list = parse.parse_qsl(url_components.query)
+        dic = dict(query_string_list)
+        
+        month, year = dic['month'], dic['year']
+        
+        if month and year:
+            message = calendar.month(year, month)
+        elif month and not year:
+            message = calendar.month(2022, month)
+        elif not month and year:
+            message = calendar.month(year, 4)
+        else:
+            message = calendar.month(2022, 4)
+        
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        
+        self.wfile.write(message.encode())
+        return
